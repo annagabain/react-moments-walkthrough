c844134b2faeb892042be00e679f39960656bcbb
Anna Gabain <anna.gabain@outlook.com>
2023-05-28 08:44:54 +0000

    The PopularProfiles Component - Part 1

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -4,19 +4,16 @@ import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
-
 import Post from "./Post";
 import Asset from "../../components/Asset";
-
 import appStyles from "../../App.module.css";
 import styles from "../../styles/PostsPage.module.css";
 import { useLocation } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
-
 import NoResults from "../../assets/no-results.png";
-
 import InfiniteScroll from "react-infinite-scroll-component";
 import { fetchMoreData } from "../../utils/utils";
+import PopularProfiles from "../profiles/PopularProfiles";
 
 
 function PostsPage({ message, filter = "" }) {
@@ -92,7 +89,7 @@ function PostsPage({ message, filter = "" }) {
         )}
       </Col>
       <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
-        <p>Popular profiles for desktop</p>
+      <PopularProfiles />
       </Col>
     </Row>
   );
